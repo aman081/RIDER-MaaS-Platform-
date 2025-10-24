6707c87f7e9d232679924c73ae9d9fe8aa07ce33
aman081 <aman8987668356@gmail.com>
2025-10-24 17:38:33 +0530

    changed start page

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Start = () => {
   return (
     <div>
-      <div className='h-screen bg-cover bg-bottom bg-[url(https://imgs.search.brave.com/Wy-f3ZTfp-UvHFPWIgW9yKU9cG78epYySJMMH-9zTHI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/Y2l0eXBuZy5jb20v/cHVibGljL3VwbG9h/ZHMvcHJldmlldy9o/ZC0zZC10YXhpLWNh/Yi1jYXItdmVoaWNs/ZS1wbmctNzA0MDgx/Njk0ODc0MTEwemFv/OWpiaXBjZC5wbmc_/dj0yMDI1MDkxMjIy)] flex justify-between flex-col  w-full bg-red-400'>
+      <div className='h-screen bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1572013343866-dfdb9b416810?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGF4aXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600)] flex justify-between flex-col  w-full bg-red-400'>
        <img className='w-16 ml-8' src="https://imgs.search.brave.com/yjzGFf2Us28osYOCiZ6SYSGUGswyR6tspkw91KDlljw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/cG5nbWFydC5jb20v/ZmlsZXMvMjIvQ2Fy/LUxvZ28tUE5HLVBp/Yy5wbmc"/>
         <div className='bg-white flex-col py-4 px-4 pb-7'>
             <h2 className='text-3xl font-bold'>Get Started with SAARTHI</h2>
